d06ce0cfb3df7e30b3faf6655d5ef7d8e9dd6700
EleanorStrotz <EleanorStrotz@gmail.com>
2014-12-02 12:23:27 -0800

    adding sticky menu

--- js/main.js
+++ js/main.js
@@ -40,3 +40,14 @@ function parallaxIt() {
 };
 
 parallaxIt();
+
+// Create a clone of the menu, right next to original.
+$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+
+function stickIt() {
+
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
